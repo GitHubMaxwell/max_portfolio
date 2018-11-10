120587dbb7141a9dbf102e3af6b793edaab9c7c9
Maxwell Rediker <maxwell.rediker@gmail.com>
2018-11-09 21:01:15 -0800

    filling in information

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-// can be func component??
-
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <main id="about">
-        <h1 className="lg-heading">
-          Contact <span className="text-secondary">Me</span>
-        </h1>
-        <h2 className="sm-heading">Let's Get In Touch!</h2>
-        <div className="boxes">
-          <div>
-            <span className="text-secondary">Email:</span>johndoe@test.com
-          </div>
-          <div>
-            <span className="text-secondary">Phone:</span>555-555-5555
-          </div>
-          <div>
-            <span className="text-secondary">Address:</span>Seattle,WA
-          </div>
+export default function Contact() {
+  return (
+    <main id="about">
+      <h1 className="lg-heading">
+        Contact <span className="text-secondary">Me</span>
+      </h1>
+      <h2 className="sm-heading">Let's Get In Touch!</h2>
+      <div className="boxes">
+        <div>
+          <span className="text-secondary">Email: </span>johndoe@test.com
         </div>
-      </main>
-    );
-  }
+        {/* <div>
+            <span className="text-secondary">Phone:</span>555-555-5555
+          </div> */}
+        <div>
+          <span className="text-secondary">Address: </span>Seattle,WA
+        </div>
+      </div>
+    </main>
+  );
 }
